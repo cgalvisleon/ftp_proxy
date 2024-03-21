213ed5a4462967528f7d55d22b7d4fd28792a731
Cesar Galvis Leon <cgalvisleon@gmail.com>
2024-03-21 15:02:44 -0500

    Update

--- update.sh
+++ update.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # stop the running containers
-# docker-compose -p ftp-proxy -f ./proxy.yml down
+docker-compose -p ftp-proxy -f ./proxy.yml down
 
 # pull the latest changes from the remote repository
-# git get
+git get
 
 # build the new image
 docker build --no-cache -t my-proxy -f ./Dockerfile .
