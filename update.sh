5a825d45bfbff7fb0407e30c830932c0761e7cf9
Cesar Galvis Leon <cgalvisleon@gmail.com>
2024-03-21 14:59:16 -0500

    Update

--- update.sh
+++ update.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # stop the running containers
-docker-compose -p my-nginx -f ./nginx.yml down
+# docker-compose -p ftp-proxy -f ./proxy.yml down
 
 # pull the latest changes from the remote repository
-git get
+# git get
 
 # build the new image
-docker build --no-cache -t my-nginx -f ./Dockerfile .
+docker build --no-cache -t my-proxy -f ./Dockerfile .
 
 # start the containers
-docker-compose -p my-nginx -f ./nginx.yml up -d
+docker-compose -p ftp-proxy -f ./proxy.yml up -d
 
 # list the running containers
 docker ps -a
